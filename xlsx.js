88b12c6aabcc51fea519c015a5451f91c7d29549
northernCold <1357805398@qq.com>
2019-03-23 16:22:19 +0800

    Update xlsx.js

--- xlsx.js
+++ xlsx.js
@@ -7874,6 +7874,19 @@ function write_ws_xml_data(ws, opts, idx, wb) {
       if (ws[ref] === undefined) continue;
       if ((cell = write_ws_xml_cell(ws[ref], ref, ws, opts, idx, wb)) != null) r.push(cell);
     }
+    if(r.length > 0) {
+      params = ({r:rr});
+      if(rows && rows[R]) {
+        row = rows[R];
+	if(row.hidden) params.hidden = 1;
+	height = -1;
+	if(row.hpx) height = px2pt(row.hpx);
+	else if(row.hpt) height = row.hpt;
+	if(height > -1) { params.ht = height; params.customHeight = 1;}
+	if(row.level) { params.outlineLevel = row.level;}
+      }
+      o[o.length] = (writextag('row',r.join(""),params));
+    }
     if (r.length > 0) o[o.length] = (writextag('row', r.join(""), {r: rr}));
   }
   return o.join("");
